220962c6625097967a85d4758adf34e63d21ed80
Thibaut Colar <tcolar@colar.net>
2014-01-06 14:08:19 -0800

    Many more methods and tests

--- slice.go
+++ slice.go
@@ -6,18 +6,28 @@ import (
 	"reflect"
 )
 
+// Custom "Generic" (Sorta) slice
+// Note: Satisfies sort.Interface so can use sort, search etc...
 type Slice struct {
+	// A comparator function. Returns 0 if a==b; -1 if a < b; 1 if a>b
+	// **NOT INITIALIZED**, must be set for sorting to work
+	Compare func(a, b interface{}) int
+
+	// Returns whether two items are equal
+	// Default imlementation uses reflect.DeepEqual
+	Equals func(a, b interface{}) bool
+
 	// internal slice that hold the objects
 	slice []interface{}
 	// value of pointer to slice
 	sliceValPtr reflect.Value
 }
 
-// Create a new empty slice
+// Initialize a new empty slice
 func NewSlice() *Slice {
 	s := &Slice{}
-	s.slice = []interface{}{}
 	s.sliceValPtr = reflect.ValueOf(&s.slice)
+	s.Equals = func(a, b interface{}) bool { return reflect.DeepEqual(a, b) }
 	return s
 }
 
@@ -46,41 +56,105 @@ func (s *Slice) Append(more interface{}) {
 	s.slice = append(s.slice, more)
 }
 
-// Append several values (slice of)
-func (s *Slice) AppendAll(more interface{}) {
-	v := reflect.ValueOf(more)
-	// assert v.Kind is Slice ?
-	for i := 0; i != v.Len(); i++ {
-		s.Append(v.Index(i).Interface())
-	}
+// Append several values
+func (s *Slice) AppendAll(more ...interface{}) {
+	s.slice = append(s.slice, more...)
 }
 
-// Concatenate this slice with vaues of another slice
-// TODO
-//func (s *Slice) Concat(slice interface{}) {
-//}
+// Append a goon.Slice to this slice
+func (s *Slice) AppendSlice(slice *Slice) {
+	s.slice = append(s.slice, slice.slice...)
+}
+
+// Current slice capacity
+func (s *Slice) Cap() int {
+	return cap(s.slice)
+}
+
+// Clear (empty) the list
+func (s *Slice) Clear() {
+	// Note: A nil slice in go is valid and can then be used just as if empty
+	s.slice = nil
+}
+
+// Does the slice contain the given element (by equality)
+// Note, this uses simple iteration, use sort methods if meeding more performance
+func (s *Slice) Contains(elem interface{}) bool {
+	return s.Index(elem) != -1
+}
+
+// Does the slice contain all the given values
+func (s *Slice) ContainsAll(elems ...interface{}) bool {
+	for _, elem := range elems {
+		if !s.Contains(elem) {
+			return false
+		}
+	}
+	return true
+}
 
 // Set value of ptr to this slice first element
 func (s *Slice) First(ptr interface{}) {
+	s.Get(0, ptr)
+}
+
+// Set value of ptr to slice[idx]
+// If idx is negative then idx element from the end -> slice[len(slice)+idx]
+// ie Get(-1) would return the last element
+func (s *Slice) Get(idx int, ptr interface{}) {
+	if idx < 0 {
+		idx = s.Len() + idx
+	}
 	obj := reflect.ValueOf(ptr).Elem()
-	obj.Set(reflect.Indirect(s.sliceValPtr).Index(0).Elem())
+	obj.Set(reflect.Indirect(s.sliceValPtr).Index(idx).Elem())
+}
+
+// Return the (lowest) index of given element (by equality)
+// Return -1 if the lement is part of the slice
+// Note, this uses simple iteration, use sort methods if meeding more performance
+func (s *Slice) Index(val interface{}) int {
+	for i, e := range s.slice {
+		if s.Equals(e, val) {
+			return i
+		}
+	}
+	return -1
 }
 
 // Set value of ptr to this slice last element
 func (s *Slice) Last(ptr interface{}) {
-	obj := reflect.ValueOf(ptr).Elem()
-	obj.Set(reflect.Indirect(s.sliceValPtr).Index(len(s.slice) - 1).Elem())
+	s.Get(-1, ptr)
 }
 
 // Length of this slice
+// Also used as impl of sort.Interface
 func (s *Slice) Len() int {
 	return len(s.slice)
 }
 
-func (s *Slice) Test(ptr interface{}) {
-	obj := reflect.ValueOf(ptr).Elem()
-	t := reflect.SliceOf(reflect.TypeOf(5))
-	l := reflect.MakeSlice(t, 2, 2)
-	// then fill it
-	obj.Set(reflect.Indirect(l))
+// Check if a < b (used as impl of sort.Interface)
+// S.Compare must be defined !
+func (s Slice) Less(a, b int) bool {
+	if s.Compare == nil {
+		panic("Slice.Compare function was not implemented !")
+	}
+	return s.Compare(s.slice[a], s.slice[b]) == -1
 }
+
+// Returns pointer to the underlying slice ([]interface{})
+func (s *Slice) Slice() *[]interface{} {
+	return &s.slice
+}
+
+// Swap 2 elements (used as impl of sort.Interface)
+func (s *Slice) Swap(a, b int) {
+	s.slice[a], s.slice[b] = s.slice[b], s.slice[a]
+}
+
+/*func (s *Slice) Test(ptr interface{}) {
+  obj := reflect.ValueOf(ptr).Elem()
+  t := reflect.SliceOf(reflect.TypeOf(5))
+  l := reflect.MakeSlice(t, 2, 2)
+  // then fill it
+  obj.Set(reflect.Indirect(l))
+}*/
--- slice_test.go
+++ slice_test.go
@@ -4,14 +4,15 @@ package goon
 
 import (
 	"github.com/smartystreets/goconvey/convey"
-	//"log"
+	"log"
+	"sort"
 	"testing"
 )
 
 func TestSlice(t *testing.T) {
 	// test slice
 	s := NewSlice()
-	s.AppendAll([]int{1, 2, 3})
+	s.AppendAll(1, 2, 3)
 	s.Append(7)
 	s.Append(9)
 	s.Append(15)
@@ -41,6 +42,26 @@ func TestSlice(t *testing.T) {
 		convey.So(s.Any(f4), convey.ShouldEqual, false)
 	})
 
+	convey.Convey("Get", t, func() {
+		s.Get(2, &result)
+		convey.So(result, convey.ShouldEqual, 3)
+		s.Get(-2, &result)
+		convey.So(result, convey.ShouldEqual, 9)
+	})
+
+	convey.Convey("Index", t, func() {
+		convey.So(s.Index(7), convey.ShouldEqual, 3)
+		convey.So(s.Index(1), convey.ShouldEqual, 0)
+		convey.So(s.Index(999), convey.ShouldEqual, -1)
+	})
+
+	convey.Convey("Contains", t, func() {
+		convey.So(s.Contains(7), convey.ShouldEqual, true)
+		convey.So(s.Contains(999), convey.ShouldEqual, false)
+		convey.So(s.ContainsAll(7, 1, 15), convey.ShouldEqual, true)
+		convey.So(s.ContainsAll(7, 999), convey.ShouldEqual, false)
+	})
+
 	convey.Convey("Len", t, func() {
 		convey.So(s.Len(), convey.ShouldEqual, 6)
 	})
@@ -51,6 +72,58 @@ func TestSlice(t *testing.T) {
 		s.Last(&result)
 		convey.So(result, convey.ShouldEqual, 15)
 	})
+
+	convey.Convey("Sort & Search", t, func() {
+		// Example of sorting the slice
+		s.Compare = func(a, b interface{}) int {
+			ai := a.(int)
+			bi := b.(int)
+			if ai == bi {
+				return 0
+			}
+			if ai > bi {
+				return 1
+			}
+			return -1
+		}
+		sort.Sort(s)
+		last := -9999
+		for i := 0; i != s.Len(); i++ {
+			s.Get(i, &result)
+			convey.So(result, convey.ShouldBeGreaterThanOrEqualTo, last)
+			last = result
+		}
+		// Example of using standard search on sorted data
+		i := sort.Search(s.Len(), func(i int) bool {
+			var v int
+			s.Get(i, &v)
+			log.Print(v)
+			return v >= 2 // looking for first index of "2"
+		})
+		convey.So(i, convey.ShouldEqual, 1) // should be the second element
+	})
+
+	convey.Convey("Clear", t, func() {
+		s.Clear()
+		convey.So(s.Len(), convey.ShouldEqual, 0)
+		s.Append(7)
+		convey.So(s.Len(), convey.ShouldEqual, 1)
+	})
+
+	convey.Convey("Append", t, func() {
+		s.Clear()
+		s.Append(5)
+		convey.So(s.Len(), convey.ShouldEqual, 1)
+		s.AppendAll(10, 22, 33)
+		convey.So(s.Len(), convey.ShouldEqual, 4)
+		s2 := NewSlice()
+		s2.AppendAll(12, 13)
+		s.AppendSlice(s2)
+		convey.So(s.Len(), convey.ShouldEqual, 6)
+		s.Get(-1, &result)
+		convey.So(result, convey.ShouldEqual, 13)
+	})
+
 }
 
 func BenchmarkSlice(b *testing.B) {
